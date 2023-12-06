51861d113132216c71fbe79ab2698d37bba0d6a9
Andy Rothwell <Andy.Rothwell@phila.gov>
2023-12-06 12:02:36 -0500

    starts to add infrastructure for putting in translations

---
+++ src/i18n/en-us.js
@@ -0,0 +1,34 @@
+
+export default {
+  language: 'English',
+  app: {
+    title: 'Resources for immigrants',
+    subtitle: 'Find services for immigrants in Philadelphia',
+    noResults: 'We\'re sorry, there are no results for that search. Adjust the filters you\'ve selected and try again.',
+    searchPlaceholders: {
+      all: 'Search by address',
+    },
+  },
+  introPage: {
+    section1Title: "About this finder",
+    p0: "This tool can help you find organizations that offer immigrant-focused services in Philadelphia. You can:",
+    ul1: {
+      li: "Browse the list of organizations.",
+      li2: "Search by address or keyword.",
+      li3: "Filter your results by service type.",
+    },
+    section2Title: "Adding your organization",
+    p1: "Does your organization offer services for immigrants? Contact the Office of Immigrant Affairs at <a target='_blank' href='mailto:OIA@phila.gov'>OIA@phila.gov</a> to learn how to be added to this finder.",
+  },
+  'Arts & culture': 'Arts & culture',
+  'Citizenship application help': 'Citizenship application help',
+  'English (ESL) classes': 'English (ESL) classes',
+  'Health/mental health services': 'Health/mental health services',
+  'Housing assistance': 'Housing assistance',
+  'Immigrant advocacy': 'Immigrant advocacy',
+  'Immigrant focused youth programs': 'Immigrant focused youth programs',
+  'Job readiness': 'Job readiness',
+  'Legal services': 'Legal services',
+  'Refugee/asylee services': 'Refugee/asylee services',
+  'Victim/witness support': 'Victim/witness support',
+};
---
+++ src/i18n/es.js
@@ -0,0 +1,34 @@
+
+export default {
+  language: 'Spanish',
+  app: {
+    title: 'Spanish Resources for immigrants',
+    subtitle: 'Spanish Find services for immigrants in Philadelphia',
+    noResults: 'We\'re sorry, there are no results for that search. Adjust the filters you\'ve selected and try again.',
+    searchPlaceholders: {
+      all: 'Search by address',
+    },
+  },
+  introPage: {
+    section1Title: "Spanish About this finder",
+    p0: "Spanish This tool can help you find organizations that offer immigrant-focused services in Philadelphia. You can:",
+    ul1: {
+      li: "Spanish Browse the list of organizations.",
+      li2: "Spanish Search by address or keyword.",
+      li3: "Spanish Filter your results by service type.",
+    },
+    section2Title: "Spanish Adding your organization",
+    p1: "Spanish Does your organization offer services for immigrants? Contact the Office of Immigrant Affairs at <a target='_blank' href='mailto:OIA@phila.gov'>OIA@phila.gov</a> to learn how to be added to this finder.",
+  },
+  'Arts & culture': 'Spanish Arts & culture',
+  'Citizenship application help': 'Spanish Citizenship application help',
+  'English (ESL) classes': 'Spanish English (ESL) classes',
+  'Health/mental health services': 'Spanish Health/mental health services',
+  'Housing assistance': 'Spanish Housing assistance',
+  'Immigrant advocacy': 'Spanish Immigrant advocacy',
+  'Immigrant focused youth programs': 'Spanish Immigrant focused youth programs',
+  'Job readiness': 'Spanish Job readiness',
+  'Legal services': 'Spanish Legal services',
+  'Refugee/asylee services': 'Spanish Refugee/asylee services',
+  'Victim/witness support': 'Spanish Victim/witness support',
+};
