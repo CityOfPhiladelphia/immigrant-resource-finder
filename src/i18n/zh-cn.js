a7e3759577e69d971960b0045ad6fb00969bbc3f
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-08-28 15:31:34 -0400

    adds chinese

--- src/i18n/zh-cn.js
+++ src/i18n/zh-cn.js
@@ -1,22 +1,22 @@
 export default {
-  language: '中文',
+  language: "中文",
   app: {
-    title: '欢迎社区资源搜索器',
-    subtitle: '为费城的移民寻找服务',
-    noResults: '很抱歉，没有针对该搜索的结果。 请调整所选的过滤器，然后重试。',
-    servicesOffered: '提供的服务',
+    title: "欢迎社区资源搜索器",
+    subtitle: "为费城的移民寻找服务",
+    noResults: "很抱歉，没有针对该搜索的结果。 请调整所选的过滤器，然后重试。",
+    servicesOffered: "提供的服务",
     searchPlaceholders: {
-      all: "按地址、邮政编码或关键字搜索",
+      all: "按地址、邮政编码或关键字搜索"
     },
     cityOfPhiladelphia: "费城市政府",
     about: "关于本查找器",
     betaTag: "Beta",
     feedback: "反馈",
-    map: '地图',
-    list: '清单',
+    map: "地图",
+    list: "清单",
     viewMap: "查看地图",
     viewList: "查看列表",
-    viewAccessible: "查看机构地点列表",
+    viewAccessible: "查看机构地点列表"
   },
   introPage: {
     section1Title: "查找您附近的服务和支持",
@@ -25,7 +25,7 @@ export default {
     ul1: {
       li: "按您正在寻找的服务进行筛选。",
       li2: "在地图上选择一个位置以了解详细信息。",
-      li3: "访问之前，请通过电话或电子邮件联系该组织。",
+      li3: "访问之前，请通过电话或电子邮件联系该组织。"
     },
     section2Title: "语言访问权限",
     p2: "大多数地点以英语提供大部分服务。其他语言因组织而异。要查看组织使用的语言，请打开该地点的详细信息。",
@@ -33,20 +33,20 @@ export default {
     p3_1: "此处列出的许多合作伙伴都<a href='https://www.phila.gov/oia/resource-finder/?services=Legal%20services'>提供免费安全的法律服务。</a>",
     p3_2: "当心利用客户优势的无牌移民服务提供商。在美国，除非公证人也是持牌律师，否则不允许提供法律咨询。要举报欺诈行为或投诉提供商，<a target='_blank' href='https://www.phila.gov/services/property-lots-housing/submit-a-service-request-with-311/'>请联系 311</a>。",
     section4Title: "将您的组织添加到列表中",
-    p4: "该网站由移民事务<a target='_blank' href='https://www.phila.gov/departments/office-of-immigrant-affairs/'>办公室</a>维护。如果您提供移民服务并希望被添加到此列表中，请<a target='_blank' href='mailto:OIA{'@'}phila.gov'>通过 OIA{'@'}phila.gov</a> 联系我们。",
+    p4: "该网站由移民事务<a target='_blank' href='https://www.phila.gov/departments/office-of-immigrant-affairs/'>办公室</a>维护。如果您提供移民服务并希望被添加到此列表中，请<a target='_blank' href='mailto:OIA{'@'}phila.gov'>通过 OIA{'@'}phila.gov</a> 联系我们。"
   },
-  'Arts & culture': '艺术与文化',
-  'Citizenship application help': '公民身份申请帮助',
-  'English (ESL) classes': '英语 (ESL) 课程',
-  'Health/mental health services': '健康/心理健康服务',
-  'Housing assistance': '住房援助',
-  'Immigrant advocacy': '移民倡导',
-  'Immigrant focused youth programs': '以移民为中心的青年项目',
-  'Job readiness': '工作准备',
-  'Legal services': '法律服务',
-  'Refugee/asylee services': '难民/寻求庇护者服务',
-  'Victim/witness support': '受害者/证人支持',
-  'Citizenship classes': '公民身份课程',
-  'Tax preparation': '税务准备',
-  'Education': '教育',
-};
+  "Arts & culture": "艺术与文化",
+  "Citizenship application help": "公民身份申请帮助",
+  "English (ESL) classes": "英语 (ESL) 课程",
+  "Health/mental health services": "健康/心理健康服务",
+  "Housing assistance": "住房援助",
+  "Immigrant advocacy": "移民倡导",
+  "Immigrant focused youth programs": "以移民为中心的青年项目",
+  "Job readiness": "工作准备",
+  "Legal services": "法律服务",
+  "Refugee/asylee services": "难民/寻求庇护者服务",
+  "Victim/witness support": "受害者/证人支持",
+  "Citizenship classes": "公民身份课程",
+  "Tax preparation": "税务准备",
+  "Education": "教育"
+}
