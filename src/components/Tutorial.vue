a4052291a5137c539cb0841673370d0d83ed6c95
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-09-05 15:53:14 -0400

    uses pinboard 2.3.2, adds tutorials

---
+++ src/components/Tutorial.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+
+// use these if running off unlinked package
+// import { useMainStore } from '@phila/pinboard';
+// OR
+// use this if running off linked package
+import { useMainStore } from '../../node_modules/@phila/pinboard/src/stores/MainStore.js';
+import { onMounted } from 'vue';
+
+const MainStore = useMainStore();
+
+onMounted(() => {
+  console.log('Tutorial component mounted');
+  MainStore.firstRouteLoaded = true;
+});
+
+</script>
+
+<template>
+  <!-- <img :src="`/tutorial/${$route.params.file}`" style="max-width: 100%; height: auto;" /> -->
+</template>
