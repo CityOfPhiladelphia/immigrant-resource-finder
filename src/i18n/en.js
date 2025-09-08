b187026b0ea30de790ad78ea188b3b50a02e115c
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-09-08 13:27:48 -0400

    starts renaming language files

---
+++ src/i18n/en.js
@@ -0,0 +1,52 @@
+export default{
+  "language": "English",
+  "app": {
+    "title": "Welcoming Community Resource Finder",
+    "subtitle": "Find services for immigrants in Philadelphia",
+    "noResults": "We're sorry, there are no results for that search. Adjust the filters you've selected and try again.",
+    "servicesOffered": "Services offered",
+    "searchPlaceholders": {
+      "all": "Search by address, zipcode, or keyword"
+    },
+    "cityOfPhiladelphia": "City of Philadelphia",
+    "about": "About this finder",
+    "betaTag": "Beta",
+    "feedback": "Feedback",
+    "map": "Map",
+    "list": "List",
+    "viewMap": "View map",
+    "viewList": "View list",
+    "viewAccessible": "View accessible list of site locations"
+  },
+  "introPage": {
+    "section1Title": "Find services and support near you",
+    "p0": "Philadelphia has many resources for immigrants and refugees. You can get help with legal issues, attend cultural events, sign up for English classes, and more.  Use this tool to find organizations near you.",
+    "p1": "To find immigrant and refugee resources:",
+    "ul1": {
+      "li": "Filter by the services you're looking for.",
+      "li2": "Select a location on the map for details.",
+      "li3": "Contact the organization by phone or email before visiting."
+    },
+    "section2Title": "Language access",
+    "p2": "Most locations offer most of their services in English. Other languages vary by organization. To see the languages spoken at an organization, open that location's details.",
+    "section3Title": "Immigration assistance and notario fraud",
+    "p3_1": "Many of our partners listed here provide <a href='https://www.phila.gov/oia/resource-finder/?services=Legal%20services'>free, safe legal services.</a>",
+    "p3_2": "Beware of unlicensed immigration service providers who take advantage of their customers. In the United States, a notary is not allowed to give legal advice unless they are also a licensed attorney. To report fraud or complain about a provider, <a target='_blank' href='https://www.phila.gov/services/property-lots-housing/submit-a-service-request-with-311/'>contact 311</a>.",
+    "section4Title": "Adding your organization to the list",
+    "p4": "The <a target='_blank' href='https://www.phila.gov/departments/office-of-immigrant-affairs/'>Office of Immigrant Affairs</a> maintains this website. Contact us at <a target='_blank' href='mailto:OIA{'@'}phila.gov'>OIA{'@'}phila.gov</a> if you offer immigrant services and would like to be added to this list."
+  },
+  "Arts & culture": "Arts & culture",
+  "Citizenship application help": "Citizenship application help",
+  "English (ESL) classes": "English (ESL) classes",
+  "Health/mental health services": "Health/mental health services",
+  "Housing assistance": "Housing assistance",
+  "Immigrant advocacy": "Immigrant advocacy",
+  "Immigrant focused youth programs": "Immigrant focused youth programs",
+  "Job readiness": "Job readiness",
+  "Legal services": "Legal services",
+  "Refugee/asylee services": "Refugee/asylee services",
+  "Victim/witness support": "Victim/witness support",
+  "Citizenship classes": "Citizenship classes",
+  "Tax preparation": "Tax preparation",
+  "Education": "Education"
+}
